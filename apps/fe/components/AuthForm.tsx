7187a2cb84bc3407fc88c4c081663d2d4a6bfcf8
Jaya <jaya.dev100x@gmail.com>
2025-07-16 18:15:53 +0530

    introduced hook-form,zod resolvers

--- apps/fe/components/AuthForm.tsx
+++ apps/fe/components/AuthForm.tsx
@@ -1,40 +1,59 @@
 "use client"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import FormButton from "./FormButton";
-import SigninForm from "./Signin";
-import SignupForm from "./Signup";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import InputBox from "./FormInputBox";
+import {signinSchema, signupSchema} from '@repo/zod/schema'
+
 
 
 export default function AuthForm(){
 
-    const [loginState, setLoginState] = useState("Signin")
+    const [loginState, setLoginState] = useState("Sign in")
+    const currentSchema = useMemo(() => { return (loginState === "Sign in" ? signinSchema : signupSchema)}, [loginState])
+
+    const {register, handleSubmit,formState: { errors } } = useForm({resolver: zodResolver(currentSchema)});
+
+    const onSubmitHandler = () => {
+
+    }
 
     return (   
         <div className='min-h-screen flex items-center justify-center'>
           <div className='border-transparent bg-white/30 rounded-2xl shadow-2xl'>
-            <div className="flex flex-col justify-center items-center p-10">
+            <div className="flex flex-col p-10">
               
-              {loginState === "Signin" 
-                ? <SigninForm/>
-                : <SignupForm/>
-              }
-            
-              {loginState === "Signin" ?
-                <div className="text-sm ">
-                  Dont have an account? <a onClick={() => setLoginState("Signup")}> Sign up </a>
-                </div>
-              : <div>
-                  Already have an account? <a onClick={() => setLoginState("Signin")}> SignIn </a>
-                </div>
-              }
+              <form key={loginState} onSubmit={() => {}}>
 
-              <div className="flex items-center w-full my-4 ">
-                <div className="flex-grow h-px bg-deep-purple"/>
-                    <span className="mx-1 text-gray-700 font-medium text-xs">OR</span>
-                <div className="flex-grow h-px bg-deep-purple"/>
-              </div>
+                {loginState === "Sign up" 
+                ? <InputBox  placeholder="Name" type="text" viewIcon={false}/>
+                : null}
 
-              <FormButton width="full" text="Sign in with Google"/>
+                <div className="flex flex-col justify-center items-center">
+                  <InputBox  placeholder="Email" type="text" viewIcon={false}/>
+                  <InputBox  placeholder="Password" type="password" viewIcon={true}/>
+                  <FormButton width={30} text={loginState}/>
+                </div>
+               
+              
+                {loginState === "Sign in" ?
+                  <div className="text-sm">
+                    Dont have an account? <a onClick={() => setLoginState("Sign up")}> Sign up </a>
+                  </div>
+                : <div className="text-sm">
+                    Already have an account? <a onClick={() => setLoginState("Sign in")}> Sign In </a>
+                  </div>
+                }
+
+                <div className="flex items-center w-full my-4 ">
+                  <div className="flex-grow h-px bg-deep-purple"/>
+                      <span className="mx-1 text-gray-700 font-medium text-xs">OR</span>
+                  <div className="flex-grow h-px bg-deep-purple"/>
+                </div>
+
+                <FormButton width="full" text="Sign in with Google"/>
+              </form>
             </div>
           </div>
         </div>        
--- apps/fe/components/HomeButton.tsx
+++ apps/fe/components/HomeButton.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link"
+
 export default function LoginButton(){
     return (
-        <button className="border-0 border-transparent rounded-3xl w-27 h-11 hover:[animation:border-rotate_3s_linear_infinite,green-glow-pulse_2s_ease-in-out_infinite] bg-conic/[from_var(--border-angle)] from-black via-purple via-70% to-black ">
-            <span className=" px-8 py-3 bg-deep-purple text-white border-transparent border-0 rounded-full">Login</span>
-            
-        </button>
+        <Link href='/signin' className="block relative border-0 border-transparent rounded-3xl w-27 h-11 hover:[animation:border-rotate_2s_linear_infinite,green-glow-pulse_2s_ease-in-out_infinite] bg-conic/[from_var(--border-angle)] from-black via-purple via-80% to-black flex justify-center items-center" >
+            <span className=" px-8 py-2 bg-deep-purple text-white border-transparent border-0 rounded-full">Login</span>    
+        </Link>
     )
 }
 
